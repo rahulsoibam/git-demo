480108fe2760cdc512be4e54105ad610d1c2dbca
Rahul Soibam <rahulsoibam@gmail.com>
2022-01-24 18:43:52 +0530

    Printed names, added sqrt functions

---
+++ index.js
@@ -0,0 +1,11 @@
+// console.log("Hello world");
+
+// console.log(1793913 * 12);
+
+names = ["Ritesh", "Dorendro", "Bekki", "Bimananda", "Sanjeev"];
+
+for (let i = 0; i < 5; i++) {
+  console.log(names[i]);
+}
+
+console.log(Math.sqrt(1213213123));
